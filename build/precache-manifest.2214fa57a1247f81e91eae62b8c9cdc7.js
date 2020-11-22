ae1a647d00eeb99328b9d8432e1d1bf6760d2913
deCha Sanson <deCha92@hotmail.com>
2020-11-22 12:52:13 -0600

    Added build folder and redirects file

---
+++ build/precache-manifest.2214fa57a1247f81e91eae62b8c9cdc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb23f54f28cbfd1eaf66b9cc3044de90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b338d221abf1d351f24",
+    "url": "/static/js/2.7d20fcbf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7d20fcbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597302161427af389c08",
+    "url": "/static/js/main.d458ede9.chunk.js"
+  },
+  {
+    "revision": "f3f8fb5c825638ea9727",
+    "url": "/static/js/runtime-main.e93c7fde.js"
+  }
+]);
